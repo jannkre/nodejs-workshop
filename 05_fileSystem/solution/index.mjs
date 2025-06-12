8e9839fa1285c13a5310c3d430fa97d5e24c9d09
Jannis Kreis <janniskreis@googlemail.com>
2025-06-12 11:46:25 +0200

    05 corrected solution

--- 05_fileSystem/solution/index.mjs
+++ 05_fileSystem/solution/index.mjs
@@ -5,10 +5,15 @@ import { resizeImage } from "./lib/fileHandling.mjs";
 const resp = await fetch("https://api.sampleapis.com/presidents/presidents");
 const json = await resp.json();
 
-json.forEach(async (president) => {
+let args = process.argv.slice(2);
+
+let president = json.filter(president => president.name.includes(args[0]));
+
+president.forEach(async (president) => {
+  console.log(president);
   await downloadImage(president.photo, process.env.NODE_ENV === "production" ? `./files/production/${president.id}.jpg` : `./files/development/${president.id}.jpg` );
   try {
-    await resizeImage(process.env.NODE_ENV === "production" ? `./files/production/${president.id}.jpg` : `./files/development/${president.id}.jpg`, 100, 100);
+    await resizeImage(process.env.NODE_ENV === "production" ? `./files/production/${president.id}.jpg` : `./files/development/${president.id}.jpg`, parseInt(args[1]), parseInt(args[2]));
   } catch (error) {
     console.error(error);
   }
